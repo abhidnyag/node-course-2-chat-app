7099c25e1b0ae4d9a22d55c5487d2cbc344feb88
abhidnyag <abhidnyag123@gmail.com>
2017-12-18 16:20:01 +0530

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io(); //when we call this, we are actually initiating the request f
 socket.on('connect', function (){
    console.log('Connected to server');
 
-  /*  socket.emit('createEmail', {
-       to: 'jen@example.com',
-       text: 'Hey, I am Abby.'
-   }); */
-
  /*   socket.emit('createMessage', {
        to: 'Kr',
        text: 'Yeah, sure. We will talk later.'
@@ -17,10 +12,6 @@ socket.on('connect', function (){
 socket.on('disconnect', function (){
     console.log('Disconnected from server');
 });
-
-/* socket.on('newEmail', function (email) {
- console.log('New email', email);
-}); */
 
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
--- server/server.js
+++ server/server.js
@@ -14,11 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
  console.log('New user connected');
 
- /* socket.emit('newEmail', {
-     from: 'mike@example.com',
-     text: 'Hey. What is going on?',
-     createdAt: 123
- }); */
+socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
  /* socket.emit('newMessage', {
      from: 'Abhi',
@@ -26,16 +32,19 @@ io.on('connection', (socket) => {
      createdAt: 12
      }); */
 
- /* socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
- }); */
 socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+     io.emit('newMessage', {
         from: message.from,
         text: message.text,
         createdAt: new Date().getTime()
-    });
+    }); 
+
+  /*   socket.broadcast.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+    }); */
 });
 
 socket.on('disconnect', () =>{
