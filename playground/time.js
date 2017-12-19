3dbb5c77127717327ff9a150d2add40956a9048a
abhidnyag <abhidnyag123@gmail.com>
2017-12-19 12:37:26 +0530

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+/* var date = moment();
+date.add(1, 'years').subtract(9, 'months');
+console.log(date.format('MMM Do, YYYY DDDo')); */
+var date = moment();
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io(); //when we call this, we are actually initiating the request f
 
 socket.on('connect', function (){
    console.log('Connected to server');
-
- /*   socket.emit('createMessage', {
-       to: 'Kr',
-       text: 'Yeah, sure. We will talk later.'
-   }); */
 });
 
 socket.on('disconnect', function (){
@@ -14,18 +9,20 @@ socket.on('disconnect', function (){
 });
 
 socket.on('newMessage', function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a'); 
     console.log('newMessage', message);
    var li = jQuery('<li></li>');
-   li.text(`${message.from}: ${message.text}`);
+   li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
    jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
